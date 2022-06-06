03ba7161181524d37cf0e77e34631597ba7c4feb
bigblackcork <106901158+bigblackcork@users.noreply.github.com>
2022-06-06 14:33:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['4yC+Q+HuPAtIHmKSL6KjC6yA9tOiVQ5gFd8Bqhy00A+riGpkUNg/PbVwVvKs/7KABxJBQ3CmgWqB5+HsMEjSTWt6fjD4LWQ4vQtF5lLH1Ogn+5/75o7mARBRZEneMsOqDy+fNeH2+H8qzuA8O9dZXAdB04t89/1O/w1cDnyilFU='])
-handler = WebhookHandler(os.environ['caadf9bd28b0c46e8b6a6558cbd31b86'])
+line_bot_api = LineBotApi(os.environ[4yC+Q+HuPAtIHmKSL6KjC6yA9tOiVQ5gFd8Bqhy00A+riGpkUNg/PbVwVvKs/7KABxJBQ3CmgWqB5+HsMEjSTWt6fjD4LWQ4vQtF5lLH1Ogn+5/75o7mARBRZEneMsOqDy+fNeH2+H8qzuA8O9dZXAdB04t89/1O/w1cDnyilFU=])
+handler = WebhookHandler(os.environ[caadf9bd28b0c46e8b6a6558cbd31b86])
 
 @app.route("/")
 def index():
